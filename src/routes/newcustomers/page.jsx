01cceeca3cd336f8f252945697e33fe1a65f668c
saim <ahmed6913@users.noreply.github.com>
2025-06-26 16:04:38 +0530

    UI fixing

--- src/routes/newcustomers/page.jsx
+++ src/routes/newcustomers/page.jsx
@@ -28,7 +28,7 @@ export default function NewCustomerPage() {
   return (
     <div className="p-6 text-slate-900 dark:text-white">
       <h1 className="text-3xl font-bold mb-6">Add New Customer</h1>
-      <div className="bg-white dark:bg-slate-900 p-6 rounded-lg shadow-md ">
+      <div className="bg-white dark:bg-slate-900 p-6 rounded-lg shadow-md max-w-md ">
         <h2 className="text-xl font-semibold mb-4">Create New Customer</h2>
         <input
           type="text"
